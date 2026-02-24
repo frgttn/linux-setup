614b1a2dddf00c9686fd85bf48f2fe45ee2db24a
Vladyslav Hryhorenko <hryhorenkovladyslav@outlook.com>
2026-02-24 22:20:19 +0200

    update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,16 +4,16 @@ end
 
 starship init fish | source
 
-/home/frgttn/.local/bin/mise activate fish | source
+~/.local/bin/mise activate fish | source
 
 # pnpm
-set -gx PNPM_HOME "/home/frgttn/.local/share/pnpm"
+set -gx PNPM_HOME "~/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 
 # Added by LM Studio CLI (lms)
-set -gx PATH $PATH /home/frgttn/.lmstudio/bin
+set -gx PATH $PATH ~/.lmstudio/bin
 # End of LM Studio CLI section
 
